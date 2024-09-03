3520130381fc2ecfdfff861f14a282c3425b4322
SyriusGit <henridoss.miranda@gmail.com>
2024-09-03 15:39:56 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const http2 = require('http2');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+const server = http2.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
